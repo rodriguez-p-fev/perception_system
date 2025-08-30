5a880366ac204add976aefac47783b30bf7ed5a9
Pedro <rodriguez_p@fev.com>
2025-08-30 17:50:31 -0600

    config

--- src/config.py
+++ src/config.py
@@ -2,14 +2,11 @@ from os.path import isfile
 import os
 from configparser import ConfigParser
 from cli import args
-print(args.main_path)                 
-if not isfile(os.path.join(os.getcwd(), 'config.ini')):
-    print("config file doesn't exist")
-else:
-    cfgParser = ConfigParser()
-    cfgParser.read(os.path.join(os.getcwd(), 'config.ini'))
-    sections = cfgParser.sections()
-    imgs_path               = os.path.join(args.main_path, 'images')
-    segmentation_model_path = os.path.join(args.main_path, 'segmentations')
-    keypoints_model_path    = os.path.join(args.main_path, 'keypoints_detections')
-    output_path             = os.path.join(args.main_path, 'output')
+
+cfgParser = ConfigParser()
+cfgParser.read(os.path.join(os.getcwd(), 'config.ini'))
+sections = cfgParser.sections()
+imgs_path               = os.path.join(args.main_path, 'images')
+segmentation_model_path = os.path.join(args.main_path, 'segmentations')
+keypoints_model_path    = os.path.join(args.main_path, 'keypoints_detections')
+output_path             = os.path.join(args.main_path, 'output')
